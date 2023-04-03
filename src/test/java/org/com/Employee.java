e4640e95e71fe9140ee7af6c6e114a586910368d
SUDHAGAR98 <sudhagar203982gmail.com>
2023-04-03 21:15:35 +0530

    first master

---
+++ src/test/java/org/com/Employee.java
@@ -0,0 +1,65 @@
+package org.com;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Iterator;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class Employee {
+	
+private void method1() {
+	System.out.println("sudhagar");
+
+}
+private void method2() {
+	System.out.println("vijay");
+
+}
+private void method3() {
+	System.out.println("java");
+
+}
+	
+	
+	
+	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
